b725cd7873cbed1e3f39e77bee58d5d3ead1855a
Axel Luna Hernandez <axel.lu04@gmail.com>
2025-11-02 23:56:51 -0600

    Fix: Corregir puerto de la app y URI de la BD

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
+// (Este es el app.js corregido)
+
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 
 app.use(express.json());
 
-//--> const mongoDBURI = 'mongodb://usuario:pass@localhost:27017/estudiantesDB';
-const mongoDBURI = 'mongodb://localhost:27017/estudiantesDB';
+// 1. SOLUCIÓN: Usar la variable de entorno que pasamos desde docker-compose.yml
+// process.env.MONGODB_URI tendrá el valor 'mongodb://mongo:27017/estudiantesdb'
+const mongoDBURI = process.env.MONGODB_URI;
 
 mongoose.connect(mongoDBURI)
-.then(() => console.log('Conectado a MongoDB'))
+.then(() => console.log('Conectado a MongoDB exitosamente'))
 .catch(err => console.error('Error al conectar a MongoDB:', err));
 
 
 app.use('/api/estudiantes', require('./routes/estudiantes'));
 
 
-app.listen(3033, () => {
-    console.log('Servidor ejecutándose en http://localhost:3033');
+// 2. SOLUCIÓN: Escuchar en el puerto 3000,
+// que es el puerto que el Dockerfile expone.
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor ejecutándose en http://localhost:${PORT}`);
 });
